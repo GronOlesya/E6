f5b98c33befb3f8ca9d72013bcaecb47fe369bcf
GronOlesya <122802200+GronOlesya@users.noreply.github.com>
2024-01-06 18:28:28 +0300

    Delete ChatApp directory

--- ChatApp/my_chat/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import *
-
-admin.site.register(Message)
-admin.site.register(ProfilePhoto)
-admin.site.register(GroupMessage)
-admin.site.register(GroupChat)
-admin.site.register(GroupChatUser)
